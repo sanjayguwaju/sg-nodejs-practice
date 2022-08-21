1be67751ffdb39d756e255e7205e3e4c170bbc5e
sanjayguwaju <sanjayguwaju@gmail.com>
2022-08-21 13:36:12 +0545

    add adding loading saving to note.json file functionality has been done with catch and error

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const add = require('./utils.js');
 const chalk = require('chalk');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 const yargs = require('yargs');
 const { command, describe, demandOption } = require('yargs');
 
@@ -22,8 +22,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
+        notes.addNotes(argv.title, argv.body)
     }
 })
 
