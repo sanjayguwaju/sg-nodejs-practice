cc56033ef78b70d9b15e49c715f14536ac9b3369
sanjayguwaju <sanjayguwaju@gmail.com>
2022-08-21 13:39:30 +0545

    added functionalty of adding notes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,39 @@
+const fs = require('fs');
 const getNotes = function () {
-    console.log('Your notes...')
+    return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNotes = function (title, body) {
+    const notes = loadNotes()
+
+    //Below in notes.push it take title & body from above function and push to the array.
+    notes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(notes)
+
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return []
+        // When there is empty file then it return empty array 
+        //empty notes  file === empty array
+    }
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNotes
+}
