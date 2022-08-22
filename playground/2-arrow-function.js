e4694770bb5967df105c4ff936e263a0254cf178
sanjayguwaju <sanjayguwaju@gmail.com>
2022-08-22 12:17:48 +0545

    Learned Arrow functions methods

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -13,3 +13,18 @@ const square1 = (x) => {
 const square2 = (x) => x * x
 
 console.log(square(5));
+
+
+const event = {
+    name: "Birthday Party",
+    guestListName: ['Raju', 'Balay', 'Aakriti', 'Sakshi', 'Priyansha'],
+    printGuestNames() {
+        console.log('Guest list for' + this.name);
+
+        this.guestListName.forEach((guest) => {
+            console.log(guest + ' is attending ' + this.name);
+        })
+    }
+}
+
+event.printGuestNames();
