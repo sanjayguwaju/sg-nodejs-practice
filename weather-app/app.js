5ce73e4ec02a0dd18f64df66b32154f24d625ec3
sanjayguwaju <sanjayguwaju@gmail.com>
2022-09-22 12:42:45 +0545

    Refactor: use of callback function to make the code reusable and clean

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,4 @@
+/*
 const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=0c2900a2c3b674dabc3d2ae731103799&query=37.8267,-122.4233';
@@ -33,4 +34,33 @@ request({ url: geocodeURL, json: true }, (error, response) => {
 
         // console.log('The logitutude of Los Angeles is ' + response.feature.center[0] + " . The latitude f the Los Angeles is " + response.feature.center[1]);
     }
+})
+
+*/
+
+const request = require('request');
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2FuamF5Z3V3YWp1IiwiYSI6ImNsN3l1eXN6NjEwZWgzcHVicHhxcDd5enAifQ.l8wfKxMPKbzPktG2_s3-Yw&limit='
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find the location. Try another search. ', undefined)
+        } else {
+            callback(undefined, {
+                longitude: response.body.features[0].center[0],
+                latitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+
+    })
+
+}
+
+geocode('Pheladelphia', (error, data) => {
+    console.log('Error: ', error);
+    console.log('Data: ', data);
 })
