9b5716901ff22b19d138aac02b611558b4383397
sanjayguwaju <sanjayguwaju@gmail.com>
2022-09-22 12:48:26 +0545

    Code moved to another location for clean code.

--- weather-app/app.js
+++ weather-app/app.js
@@ -39,28 +39,9 @@ request({ url: geocodeURL, json: true }, (error, response) => {
 */
 
 const request = require('request');
+const geocode = require('./utils/geocode');
 
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2FuamF5Z3V3YWp1IiwiYSI6ImNsN3l1eXN6NjEwZWgzcHVicHhxcDd5enAifQ.l8wfKxMPKbzPktG2_s3-Yw&limit='
-
-    request({ url: url, json: true }, (error, response) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
-            callback('Unable to find the location. Try another search. ', undefined)
-        } else {
-            callback(undefined, {
-                longitude: response.body.features[0].center[0],
-                latitude: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
-            })
-        }
-
-    })
-
-}
-
-geocode('Pheladelphia', (error, data) => {
+geocode('Boston', (error, data) => {
     console.log('Error: ', error);
     console.log('Data: ', data);
 })
