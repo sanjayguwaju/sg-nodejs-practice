bf37863362470af7681cd16d09c4a2774ef69cd0
sanjayguwaju <sanjayguwaju@gmail.com>
2022-09-23 19:09:50 +0545

    learned: to add path for views using hbs handlebars module.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,29 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        "title": "This is homepage",
+        "name": "Weather App"
+
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        helpMessage: 'Please avoid crowd due to the Pandemic'
+    })
+})
+
 
 // app.get('', (req, res) => {
 //     res.send("<h1>Hello Express</h1>")
