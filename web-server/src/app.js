008aee51f87c7d99d2d318258ecd9b71f1c1817e
sanjayguwaju <sanjayguwaju@gmail.com>
2022-09-23 19:17:46 +0545

    learned: custom folder for views changed to template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,8 +4,10 @@ const express = require('express');
 
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 app.use(express.static(publicDirectoryPath))
 
 
