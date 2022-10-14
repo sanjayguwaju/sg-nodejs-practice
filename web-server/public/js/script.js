c62bd3fcb88d6993c45e00f6200dc35dc6cc7724
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-14 11:31:51 +0545

    able to submit the location in form and get location weather in console

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -9,17 +9,42 @@ console.log("Javascript file is fully loaded");
 // })
 
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+// fetch('http://localhost:3000/weather?address=boston').then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 
 // fetch('http://example.com/movies.json')
 //     .then((response) => response.json())
 //     .then((data) => console.log(data));
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input')
+
+// e means event 
+// e.preventDefault() prevent from refresthing
+
+// search.value will help to store the  value of input and show it in console.
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
