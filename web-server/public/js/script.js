b25e7e6ecd8a260ed59752da2b7e30900512b4a7
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-14 11:10:01 +0545

    learned: how to fetch api and use it

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -1,1 +1,25 @@
 console.log("Javascript file is fully loaded");
+
+// Below code is for to show up how  fetching api works.
+
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+// fetch('http://example.com/movies.json')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
