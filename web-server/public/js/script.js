579db42d0b0696556ad85d88e2f5cd110ad17c0c
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-14 11:57:16 +0545

    learned: show data and change the content through the fetch api

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -26,6 +26,10 @@ console.log("Javascript file is fully loaded");
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'Javascript'
 
 // e means event 
 // e.preventDefault() prevent from refresthing
@@ -36,15 +40,19 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+    messageOne.textContent = 'Loading....'
+    messageTwo.textContent = ''
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast.message
                 console.log(data.forecast)
             }
         })
     })
 })
+
