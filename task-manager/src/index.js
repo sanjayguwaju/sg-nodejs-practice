6ebe7a670fb628596ad225604bf85836b036e750
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-21 14:25:45 +0545

    Creaded endpoint for Read Post and findById

---
+++ task-manager/src/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+require('./db/mongoose')
+const Task = require('./models/task')
+const User = require('./models/user')
+
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+// This is the part of code where is got stuck for a day and i finally found out that i haven't require db/mongoose.js the database connection.
+//That's why i was not able to connect with database.
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+//Again i got stuck here /users i have only typed users/:id so i could not able to findById 
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+
+})
+
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
