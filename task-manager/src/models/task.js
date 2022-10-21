93e81dd64b7c1bb1c80f88ae189841de42302763
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-21 14:24:55 +0545

    code seprated in different folders and exported and imported according to it.

---
+++ task-manager/src/models/task.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+// const task = new Task({
+//     description: 'Learn the Mongoose Library',
+//     completed: false
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+module.exports = Task
---
+++ task-manager/src/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error("Password cannot contain password")
+            }
+        }
+    }
+})
+
+// const me = new User({
+//     name: 'Manik',
+//     age: 24,
+//     email: 'MANIK@gmail.com    ',
+//     password: 'Manik12345     '
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+module.exports = User
