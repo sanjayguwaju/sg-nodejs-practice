524035163a1b0f568c5333bfd58c30e54cf084c2
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-21 21:15:41 +0545

    implemented async

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -12,74 +12,136 @@ app.use(express.json())
 // This is the part of code where is got stuck for a day and i finally found out that i haven't require db/mongoose.js the database connection.
 //That's why i was not able to connect with database.
 
-app.post('/users', (req, res) => {
+// ---------- Below commented code is without async
+// app.post('/users', (req, res) => {
+//     const user = new User(req.body)
+
+//     user.save().then(() => {
+//         res.status(201).send(user)
+//     }).catch((e) => {
+//         res.status(400).send(e)
+//     })
+// })
+
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+// GET HTTP request without async are commented below
+
+
+// app.get('/users', (req, res) => {
+//     User.find({}).then((users) => {
+//         res.send(users)
+//     }).catch((e) => {
+//         res.status(500).send(e)
+//     })
+// })
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
+
+
+// app.get('/tasks', (req, res) => {
+//     Task.find({}).then((tasks) => {
+//         res.send(tasks)
+//     }).catch((e) => {
+//         res.status(500).send(e)
+//     })
+// })
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
 //Again i got stuck here /users i have only typed users/:id so i could not able to findById 
 
-app.get('/users/:id', (req, res) => {
+// app.get('/users/:id', (req, res) => {
+//     const _id = req.params.id
+
+//     User.findById(_id).then((user) => {
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(user)
+//     }).catch((e) => {
+//         res.status(500).send(e)
+//     })
+
+// })
+
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send()
         }
-
         res.send(user)
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
-
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
+// app.get('/tasks/:id', (req, res) => {
+//     const _id = req.params.id
+
+//     Task.findById(_id).then((task) => {
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(task)
+//     }).catch((e) => {
+//         res.status(500).send(e)
+//     })
+
+// })
+
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send()
         }
-
         res.send(task)
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
-
+    } catch (e) {
+        res.status(500).send()
+    }
 })
-
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
