831406b5779e2b4dec74a7884d470e67139d6be5
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-22 07:28:25 +0545

    all the old comments deleted and router from express is implemented to make the route seperate

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,19 +9,12 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-// This is the part of code where is got stuck for a day and i finally found out that i haven't require db/mongoose.js the database connection.
-//That's why i was not able to connect with database.
+const router = new express.Router()
+router.get('/test', (req, res) => {
+    res.send('This is from my other router')
+})
 
-// ---------- Below commented code is without async
-// app.post('/users', (req, res) => {
-//     const user = new User(req.body)
-
-//     user.save().then(() => {
-//         res.status(201).send(user)
-//     }).catch((e) => {
-//         res.status(400).send(e)
-//     })
-// })
+app.use(router)
 
 app.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -45,16 +38,6 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
-// GET HTTP request without async are commented below
-
-
-// app.get('/users', (req, res) => {
-//     User.find({}).then((users) => {
-//         res.send(users)
-//     }).catch((e) => {
-//         res.status(500).send(e)
-//     })
-// })
 
 app.get('/users', async (req, res) => {
     try {
@@ -65,16 +48,6 @@ app.get('/users', async (req, res) => {
     }
 })
 
-
-
-// app.get('/tasks', (req, res) => {
-//     Task.find({}).then((tasks) => {
-//         res.send(tasks)
-//     }).catch((e) => {
-//         res.status(500).send(e)
-//     })
-// })
-
 app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
@@ -83,22 +56,6 @@ app.get('/tasks', async (req, res) => {
         res.status(500).send(e)
     }
 })
-//Again i got stuck here /users i have only typed users/:id so i could not able to findById 
-
-// app.get('/users/:id', (req, res) => {
-//     const _id = req.params.id
-
-//     User.findById(_id).then((user) => {
-//         if (!user) {
-//             return res.status(404).send()
-//         }
-
-//         res.send(user)
-//     }).catch((e) => {
-//         res.status(500).send(e)
-//     })
-
-// })
 
 app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
@@ -114,20 +71,6 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
-// app.get('/tasks/:id', (req, res) => {
-//     const _id = req.params.id
-
-//     Task.findById(_id).then((task) => {
-//         if (!task) {
-//             return res.status(404).send()
-//         }
-
-//         res.send(task)
-//     }).catch((e) => {
-//         res.status(500).send(e)
-//     })
-
-// })
 
 app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
