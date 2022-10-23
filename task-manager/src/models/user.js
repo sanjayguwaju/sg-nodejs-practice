bccc23a6e215d576cebb16a6b36410f61914115a
sanjayguwaju <sanjayguwaju@gmail.com>
2022-10-23 14:09:32 +0545

    middleware used to patch the data through the id

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -41,6 +41,17 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+
+})
+
 const User = mongoose.model('User', userSchema)
 
 // const me = new User({
--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
 
-
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
@@ -52,7 +51,12 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
         if (!user) {
             return res.status(404).send()
