e271f3ed3eb09e52f333b90ef77ab91f087d07f8
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-13 07:26:06 +0545

    jsonwebtoken auth successful now it is able to login with generating tokens.

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -42,6 +43,12 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+    return token
+}
 
 // Check weather the email and password match with database or not
 
--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -18,7 +18,7 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send(user)
+        res.send({ user, token })
     } catch (e) {
         res.send("Email or Password is wrong")
         res.status(400).send()
