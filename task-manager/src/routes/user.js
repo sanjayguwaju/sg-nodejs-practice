f24d61bdb9f3933ef0bd127a50838871799fceff
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-14 15:24:31 +0545

    get response but res has no pwd and token it is via getPublicProfile

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -20,7 +20,7 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send({ user, token })
+        res.send({ user: user.getPublicProfile(), token })
     } catch (e) {
         res.send("Email or Password is wrong")
         res.status(400).send()
