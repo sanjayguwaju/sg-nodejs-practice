19b6d1736655d4502c8a251d3c43f042856d687e
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-14 08:15:19 +0545

    logout all user at once from all devices

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -39,8 +39,15 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
-
-
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 router.get('/users/me', auth, async (req, res) => {
 
