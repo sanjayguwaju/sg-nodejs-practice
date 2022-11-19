678a7a8bfdd6d6769d46c3d1247e3de6b4955aab
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-19 20:00:24 +0545

    remove user along with all the task created by him/her

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -105,6 +106,13 @@ userSchema.pre('save', async function (next) {
 
     next()
 
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
