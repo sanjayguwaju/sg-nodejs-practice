78f74055b8627b3cf7c3e898612944b19c714df4
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-19 19:39:23 +0545

    user can only CRUD their own task not others

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -50,6 +50,12 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
 userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
--- task-manager/src/routes/task.js
+++ task-manager/src/routes/task.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const router = new express.Router()
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 
 
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        // const tasks = await Task.find({})
+        await req.user.populate('tasks')
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send(e)
     }
 })
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    // const task = new Task(req.body)
+
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
 
     try {
         await task.save()
@@ -24,11 +31,13 @@ router.post('/tasks', async (req, res) => {
 })
 
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        // const task = await Task.findById(_id)
+        // Below line check that if the task id and owner matches or not if it doesn't then it is not going to show the task created to other users.
+        const task = await Task.findOne({ _id, owner: req.user._id })
         if (!task) {
             return res.status(404).send()
         }
@@ -40,7 +49,7 @@ router.get('/tasks/:id', async (req, res) => {
 
 
 // To update exsitng task with Patch and check the other field will not get included
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -50,16 +59,20 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
+        // const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user.id })
 
-        updates.forEach((update) => task[update] = req.body[update])
 
-        await task.save()
         // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
 
         if (!task) {
             return res.status(404).send()
         }
+
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
 
         res.send(task)
     } catch (e) {
@@ -68,9 +81,9 @@ router.patch('/tasks/:id', async (req, res) => {
 })
 
 // Delete will be used to delete the task in database
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             res.status(404).send()
