88a905da490f84a49e98a637eaa79d63abd950bd
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-19 17:06:18 +0545

    give access user only to visit its own profile & not to access other to do CRUD operations.

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -50,7 +50,7 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-userSchema.methods.getPublicProfile = function () {
+userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
 
--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -20,7 +20,7 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send({ user: user.getPublicProfile(), token })
+        res.send({ user: user, token })
     } catch (e) {
         res.send("Email or Password is wrong")
         res.status(400).send()
@@ -63,23 +63,23 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
 
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+//     try {
+//         const user = await User.findById(_id)
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
 
 // Patch is use to update the database with existing one
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -89,32 +89,33 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
+        // const user = await User.findById(req.params.id)
 
-        updates.forEach((update) => user[update] = req.body[update])
+        updates.forEach((update) => req.user[update] = req.body[update])
 
-        await user.save()
+        await req.user.save()
         // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
-        if (!user) {
-            return res.status(404).send()
-        }
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
 
-        res.send(user)
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
+        // const user = await User.findByIdAndDelete(req.user._id)
 
-        if (!user) {
-            res.status(404).send()
-        }
+        // if (!user) {
+        //     res.status(404).send()
+        // }
+        await req.user.remove()
 
-        res.send(user)
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
